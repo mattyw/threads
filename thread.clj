64f65af80cb6ed53fb2ac4324e8e42d379f3ce90
mattyw <mattyw@me.com>
2013-03-28 23:48:08 +0000

    doesn't terminate properly

--- thread.clj
+++ thread.clj
@@ -1,11 +1,24 @@
-(defn thread []
-    (Thread/sleep 10000)
-    1)
+(def num-threads 10)
 
+(defn thread [r]
+    (Thread/sleep 10000)
+    (dosync (alter r + 1)))
+
+(defn print-watch
+    [key id old new]
+    (prn key new))
+
+(defn wait [state]
+    (if (= @state num-threads)
+        (prn "Done")
+        (recur state)))
 
 (defn main [num]
-    (let [futs (for [f (range num)] (future f))
+    (let [state (agent 0)
       x 0]
-      (reduce + (map (fn [f] @f) futs))))
+      (add-watch state :prn print-watch)
+      (doseq [_ (range num)]
+        (send state inc))
+      (wait state)))
 
-(main 10000)
+(main num-threads)
