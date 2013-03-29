3dbe280d4674a8da7b841b5f7aa75362d85db359
mattyw <mattyw@me.com>
2013-03-29 12:56:38 +0000

    bad python version with lost updates

---
+++ bad.py
@@ -0,0 +1,19 @@
+import threading
+
+num = 1000000
+x = 0
+
+
+def f():
+    global x
+    x = x + 1
+    print x
+
+
+if __name__ == '__main__':
+    threads = [threading.Thread(target=f) for _ in range(num)]
+    for t in threads:
+        t.start()
+
+    print x
+    print "lost updates: ", num - x
